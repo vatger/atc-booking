e97a018d0d7c2526dbe6c337fafee992462b93c2
p.hollmann <31701319+paulhollmann@users.noreply.github.com>
2023-12-10 20:39:44 +0100

    add datahub

--- public/edll/index.php
+++ public/edll/index.php
@@ -3,6 +3,7 @@ require_once("../../conf.php");
 require_once(_BASE_PATH_ . "datamanager.class.php");
 require_once(_BASE_PATH_ . "datahub.class.php");
 require_once(_BASE_PATH_ . "imagerenderer.class.php");
+require_once(_BASE_PATH_ . "helpers.php");
 
 $MASTER_DATE = new DateTime();
 if (isset($_GET['nextWeek'])) {
